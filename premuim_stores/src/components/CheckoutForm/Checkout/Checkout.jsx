e6ef265bafb51b48f5529df839b6bdd17f002527
Bright Tenalo <debrightess@yahoo.com>
2023-01-10 22:24:24 +0100

    minor bug fix

--- premuim_stores/src/components/CheckoutForm/Checkout/Checkout.jsx
+++ premuim_stores/src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -10,10 +10,13 @@ import {
   Button,
 } from '@material-ui/core'
 
+import useStyles from './styles'
+
 const steps = ['Shipping address', 'Payment details']
 
 const Checkout = () => {
   const [activeStep, setActiveStep] = useState(0)
+  const classes = useStyles()
 
   return (
     <>
