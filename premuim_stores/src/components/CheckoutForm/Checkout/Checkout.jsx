c85ddb01b61dc0b1545b0533448e41238fc81afe
Bright Tenalo <debrightess@yahoo.com>
2023-01-11 22:15:17 +0100

    update checkout

--- premuim_stores/src/components/CheckoutForm/Checkout/Checkout.jsx
+++ premuim_stores/src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -11,6 +11,8 @@ import {
 } from '@material-ui/core'
 
 import useStyles from './styles'
+import AddressForm from '../AddressForm'
+import PaymentForm from '../PaymentForm'
 
 const steps = ['Shipping address', 'Payment details']
 
@@ -18,12 +20,18 @@ const Checkout = () => {
   const [activeStep, setActiveStep] = useState(0)
   const classes = useStyles()
 
+  const Confirmation = () => <div>Confirmation</div>
+
+  const Form = () => (activeStep === 0 ? <AddressForm /> : <PaymentForm />)
+
   return (
     <>
       <div className={classes.toolbar} />
       <main className={classes.layout}>
         <Paper className={classes.paper}>
-          <Typography variant='h4'>Checkout</Typography>
+          <Typography variant='h4' align='center'>
+            Checkout
+          </Typography>
           <Stepper activeStep={activeStep} className={classes.stepper}>
             {steps.map((step) => (
               <step key={step}>
@@ -31,6 +39,7 @@ const Checkout = () => {
               </step>
             ))}
           </Stepper>
+          {activeStep === steps.length ? <Confirmation /> : <Form />}
         </Paper>
       </main>
     </>
